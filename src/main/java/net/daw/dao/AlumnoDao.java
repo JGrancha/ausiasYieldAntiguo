948a0842ec2161dcd634e4643bd86e0a36415473
Sergio <Sergio@192.168.0.254>
2013-11-06 14:59:55 +0100

    Añado la clase "AlumnoList1.java" a net.daw.operation, y además, incluyo líneas en "AlumnoDao.java" para actualizar todos los datos que necesita en los métodos "get" y "set"

--- src/main/java/net/daw/dao/AlumnoDao.java
+++ src/main/java/net/daw/dao/AlumnoDao.java
@@ -71,10 +71,20 @@ public class AlumnoDao {
     public AlumnoBean get(AlumnoBean oAlumnoBean) throws Exception {
         try {
             oMysql.conexion(enumTipoConexion);
+            oAlumnoBean.setId_usuario(Integer.parseInt(oMysql.getOne("alumno", "id_usuario", oAlumnoBean.getId())));
+            oAlumnoBean.setDni(oMysql.getOne("alumno", "dni", oAlumnoBean.getId()));
+            oAlumnoBean.setNumexpediente(oMysql.getOne("alumno", "numexpediente", oAlumnoBean.getId()));
             oAlumnoBean.setNombre(oMysql.getOne("alumno", "nombre", oAlumnoBean.getId()));
             oAlumnoBean.setApe1(oMysql.getOne("alumno", "ape1", oAlumnoBean.getId()));
             oAlumnoBean.setApe2(oMysql.getOne("alumno", "ape2", oAlumnoBean.getId()));
+            oAlumnoBean.setSexo(oMysql.getOne("alumno", "sexo", oAlumnoBean.getId()));
+            oAlumnoBean.setDomicilio(oMysql.getOne("alumno", "domicilio", oAlumnoBean.getId()));
+            oAlumnoBean.setCodpostal(oMysql.getOne("alumno", "codpostal", oAlumnoBean.getId()));
+            oAlumnoBean.setPoblacion(oMysql.getOne("alumno", "poblacion", oAlumnoBean.getId()));
+            oAlumnoBean.setProvincia(oMysql.getOne("alumno", "provincia", oAlumnoBean.getId()));
+            oAlumnoBean.setTelefono(oMysql.getOne("alumno", "telefono", oAlumnoBean.getId()));            
             oAlumnoBean.setEmail(oMysql.getOne("alumno", "email", oAlumnoBean.getId()));
+            oAlumnoBean.setValidado(oMysql.getOne("alumno", "validado", oAlumnoBean.getId()));
             oMysql.desconexion();
         } catch (Exception e) {
             throw new Exception("AlumnoDao.getCliente: Error: " + e.getMessage());
@@ -91,10 +101,20 @@ public class AlumnoDao {
             if (oAlumnoBean.getId() == 0) {
                 oAlumnoBean.setId(oMysql.insertOne("alumno"));
             }
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "id_usuario", Integer.toString(oAlumnoBean.getId_usuario()));
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "dni", oAlumnoBean.getDni());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "numexpediente", oAlumnoBean.getNumexpediente());           
             oMysql.updateOne(oAlumnoBean.getId(), "alumno", "nombre", oAlumnoBean.getNombre());
             oMysql.updateOne(oAlumnoBean.getId(), "alumno", "ape1", oAlumnoBean.getApe1());
             oMysql.updateOne(oAlumnoBean.getId(), "alumno", "ape2", oAlumnoBean.getApe2());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "sexo", oAlumnoBean.getSexo());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "domicilio", oAlumnoBean.getDomicilio());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "codpostal", oAlumnoBean.getCodpostal());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "poblacion", oAlumnoBean.getPoblacion());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "provincia", oAlumnoBean.getProvincia());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "telefono", oAlumnoBean.getTelefono());
             oMysql.updateOne(oAlumnoBean.getId(), "alumno", "email", oAlumnoBean.getEmail());
+            oMysql.updateOne(oAlumnoBean.getId(), "alumno", "validado", oAlumnoBean.getValidado());
             oMysql.commitTrans();
         } catch (Exception e) {
             oMysql.rollbackTrans();
